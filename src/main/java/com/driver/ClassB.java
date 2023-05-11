fceb79e0eeea6e9f66aec98d59249067e4427ef6
prasadpatil079 <prasadpatil079@gmail.com>
2023-05-11 20:47:43 +0530

    updated code

--- src/main/java/com/driver/ClassB.java
+++
@@ -1,9 +0,0 @@
-package com.driver;
-
-public class ClassB extends ClassA{
-    public static String meth(){
-        return "Method is overridden in Extendend class B";
-
-    }
-
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,23 @@ import java.sql.SQLOutput;
 
 public class Main {
 
-    public static void main(String[]args){
+    public static class ClassA {
+
+        public static String meth(){
+            return "Invoking method from class A";
+
+        }
+    }
+    public static class ClassB extends ClassA {
+        //@Override
+        public static String meth(){
+            return "Method is overridden in Extendend class B";
+
+        }
+
+    }
+
+    public static void main(String[] args){
         ClassB objB=new ClassB();
         String ans=objB.meth();
 
