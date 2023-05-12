7d5207acc44d0f4c58e61a0bb9f48f92bf9b3af2
prasadpatil079 <prasadpatil079@gmail.com>
2023-05-12 11:31:28 +0530

    last commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,25 +3,19 @@ package com.driver;
 import java.sql.SQLOutput;
 
 public class Main {
-
-    public static class ClassA {
-
+    public static class A {
         public static String meth(){
             return "Invoking method from class A";
-
         }
     }
-    public static class ClassB extends ClassA {
+    public static class B extends A {
         //@Override
         public static String meth(){
             return "Method is overridden in Extendend class B";
-
         }
-
     }
-
     public static void main(String[] args){
-        ClassB objB=new ClassB();
+        B objB=new B();
         String ans=objB.meth();
 
         System.out.println(ans);
